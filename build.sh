8aba5c7bcfe967fa045878953e7bda494d2680a1
maekawatoshiki <maekawatoshiki1017@gmail.com>
2023-08-16 16:01:26 +0900

    Edit build.sh

--- build.sh
+++ build.sh
@@ -4,12 +4,15 @@
 
 export CC=/usr/lib/ccache/gcc-12 # fails with gcc-10
 
-mkdir -p build
-(
-    cd build
-    ../configure \
-        --target-list=riscv32-softmmu,riscv32-linux-user \
-        --enable-tcg \
-        --enable-plugins
-    make -j
-)
+if [ ! -d build ]; then
+    mkdir -p build
+    (
+        cd build
+        ../configure \
+            --target-list=riscv32-softmmu,riscv32-linux-user \
+            --enable-tcg \
+            --enable-plugins \
+            --enable-debug
+        make -j
+    )
+fi
